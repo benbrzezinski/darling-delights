256900f1568f11810c7608e1e0c45e2208384b44
Beniamin Brzeziński <benek9311@wp.pl>
2024-01-07 21:48:44 +0100

    Bugfixed border-radius of controllers in Feedbacks, added horizontal scroll controllers to the list in Suggestions and Instagram

--- src/components/Instagram/index.tsx
+++ src/components/Instagram/index.tsx
@@ -1,6 +1,55 @@
+import { useState, useEffect, useRef } from "react";
+import throttle from "lodash.throttle";
+import useIcons from "../../hooks/useIcons";
 import scss from "./Instagram.module.scss";
 
 const Instagram = () => {
+  const THROTTLE_DELAY = 300;
+  const [scrollState, setScrollState] = useState({
+    isScrollAtLeftMost: true,
+    isScrollAtRightMost: false,
+  });
+  const { Lower, Greater } = useIcons();
+  const productsRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      if (productsRef.current) {
+        const isScrollAtLeftMost = productsRef.current.scrollLeft === 0;
+        const isScrollAtRightMost =
+          productsRef.current.scrollLeft ===
+          productsRef.current.scrollWidth - productsRef.current.clientWidth;
+
+        setScrollState({
+          isScrollAtLeftMost,
+          isScrollAtRightMost,
+        });
+      }
+    }, THROTTLE_DELAY);
+
+    const currentProductsRef = productsRef.current;
+
+    if (currentProductsRef) {
+      currentProductsRef.addEventListener("scroll", handleScroll);
+
+      return () => {
+        currentProductsRef.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+
+  const goPrev = () => {
+    if (productsRef.current) {
+      productsRef.current.scrollBy({ top: 0, left: -280, behavior: "smooth" });
+    }
+  };
+
+  const goNext = () => {
+    if (productsRef.current) {
+      productsRef.current.scrollBy({ top: 0, left: 280, behavior: "smooth" });
+    }
+  };
+
   return (
     <div className={scss.wrapper}>
       <section className={scss.heading}>
@@ -14,104 +63,186 @@ const Instagram = () => {
           @darling
         </a>
       </section>
-      <ul className={scss.products}>
-        <li className={scss.productsItem}>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <img
-              width={250}
-              height={250}
-              src="https://i.ibb.co/ZL1j9KM/collection-7.jpg"
-              alt="product image from Instagram"
-              className={scss.productImg}
-              loading="lazy"
-            />
-          </a>
-        </li>
-        <li className={scss.productsItem}>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <img
-              width={250}
-              height={250}
-              src="https://i.ibb.co/V3tGprs/about-4.jpg"
-              alt="product image from Instagram"
-              className={scss.productImg}
-              loading="lazy"
-            />
-          </a>
-        </li>
-        <li className={scss.productsItem}>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <img
-              width={250}
-              height={250}
-              src="https://i.ibb.co/NTcQrWD/collection-13.jpg"
-              alt="product image from Instagram"
-              className={scss.productImg}
-              loading="lazy"
-            />
-          </a>
-        </li>
-        <li className={scss.productsItem}>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <img
-              width={250}
-              height={250}
-              src="https://i.ibb.co/bRK6YBX/about-5.jpg"
-              alt="product image from Instagram"
-              className={scss.productImg}
-              loading="lazy"
-            />
-          </a>
-        </li>
-        <li className={scss.productsItem}>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <img
-              width={250}
-              height={250}
-              src="https://i.ibb.co/FXqxgKT/collection-14.jpg"
-              alt="product image from Instagram"
-              className={scss.productImg}
-              loading="lazy"
-            />
-          </a>
-        </li>
-        <li className={scss.productsItem}>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            <img
-              width={250}
-              height={250}
-              src="https://i.ibb.co/bKDgk5Z/featured-product-1.jpg"
-              alt="product image from Instagram"
-              className={scss.productImg}
-              loading="lazy"
-            />
-          </a>
-        </li>
-      </ul>
+      <div style={{ position: "relative" }}>
+        <button
+          type="button"
+          style={{ display: scrollState.isScrollAtLeftMost ? "none" : "grid" }}
+          className={`${scss.controller} ${scss.prev}`}
+          onClick={goPrev}
+        >
+          <Lower className={scss.arrow} />
+        </button>
+        <ul className={scss.products} ref={productsRef}>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/ZL1j9KM/collection-7.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/V3tGprs/about-4.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/NTcQrWD/collection-13.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/bRK6YBX/about-5.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/FXqxgKT/collection-14.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/bKDgk5Z/featured-product-1.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/J2WW6N9/collection-1.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="/assets/images/heading-2.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/vvy6KXq/collection-12.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+          <li className={scss.productsItem}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              <img
+                width={250}
+                height={250}
+                src="https://i.ibb.co/SxBs6QY/collection-10.jpg"
+                alt="product image from Instagram"
+                className={scss.productImg}
+                loading="lazy"
+              />
+            </a>
+          </li>
+        </ul>
+        <button
+          type="button"
+          style={{ display: scrollState.isScrollAtRightMost ? "none" : "grid" }}
+          className={`${scss.controller} ${scss.next}`}
+          onClick={goNext}
+        >
+          <Greater className={scss.arrow} />
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Suggestions/index.tsx
+++ src/components/Suggestions/index.tsx
@@ -1,18 +1,25 @@
 import { Link, useParams, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import throttle from "lodash.throttle";
 import { toggleFavourites } from "../../redux/products/slice";
 import useProducts from "../../hooks/useProducts";
 import useIcons from "../../hooks/useIcons";
 import scss from "./Suggestions.module.scss";
 
 const Suggestions = () => {
+  const THROTTLE_DELAY = 300;
   const { id } = useParams();
+  const [scrollState, setScrollState] = useState({
+    isScrollAtLeftMost: true,
+    isScrollAtRightMost: false,
+  });
   const { products, favourites } = useProducts();
-  const { FavouritesHeartEmpty, FavouritesHeartFull } = useIcons();
+  const { FavouritesHeartEmpty, FavouritesHeartFull, Lower, Greater } =
+    useIcons();
+  const productsRef = useRef<HTMLUListElement>(null);
   const dispatch = useDispatch();
   const location = useLocation();
-  const productsRef = useRef<HTMLUListElement>(null);
 
   const product = products.find(product => product.id === id);
   const suggestedProducts = products.filter(
@@ -25,6 +32,44 @@ const Suggestions = () => {
     }
   }, [location.pathname]);
 
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      if (productsRef.current) {
+        const isScrollAtLeftMost = productsRef.current.scrollLeft === 0;
+        const isScrollAtRightMost =
+          productsRef.current.scrollLeft ===
+          productsRef.current.scrollWidth - productsRef.current.clientWidth;
+
+        setScrollState({
+          isScrollAtLeftMost,
+          isScrollAtRightMost,
+        });
+      }
+    }, THROTTLE_DELAY);
+
+    const currentProductsRef = productsRef.current;
+
+    if (currentProductsRef) {
+      currentProductsRef.addEventListener("scroll", handleScroll);
+
+      return () => {
+        currentProductsRef.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+
+  const goPrev = () => {
+    if (productsRef.current) {
+      productsRef.current.scrollBy({ top: 0, left: -306, behavior: "smooth" });
+    }
+  };
+
+  const goNext = () => {
+    if (productsRef.current) {
+      productsRef.current.scrollBy({ top: 0, left: 306, behavior: "smooth" });
+    }
+  };
+
   return product && suggestedProducts.length > 0 ? (
     <section className={scss.wrapper}>
       <div className={scss.headlineBox}>
@@ -33,63 +78,17 @@ const Suggestions = () => {
           View more
         </Link>
       </div>
-      <ul className={scss.products} ref={productsRef}>
-        {suggestedProducts.map(({ id, name, price, img }) => {
-          const isInFavourites = favourites.find(product => product.id === id);
-
-          return (
-            <li className={scss.productsItem} key={id}>
-              <div className={scss.favouritesBox}>
-                <button
-                  className={scss.favouritesBtn}
-                  type="button"
-                  onClick={() => dispatch(toggleFavourites(id))}
-                >
-                  {isInFavourites ? (
-                    <FavouritesHeartFull className={scss.favouritesIcon} />
-                  ) : (
-                    <FavouritesHeartEmpty className={scss.favouritesIcon} />
-                  )}
-                </button>
-              </div>
-              <Link
-                to={{
-                  pathname: `/shop/${id}`,
-                  search: "?from=shop&size=48&quantity=1",
-                }}
-              >
-                <div className={scss.productImgBox}>
-                  <img
-                    width={276}
-                    height={320}
-                    src={img}
-                    alt={name}
-                    className={scss.productImg}
-                    loading="lazy"
-                  />
-                </div>
-                <div className={scss.productInfoBox}>
-                  <p className={scss.productName}>{name}</p>
-                  <p className={scss.productPrice}>${price}</p>
-                </div>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  ) : (
-    <section className={scss.wrapper}>
-      <div className={scss.headlineBox}>
-        <h2 className={scss.headline}>You may also like</h2>
-        <Link to="/shop" className={scss.link}>
-          View more
-        </Link>
-      </div>
-      <ul className={scss.products} ref={productsRef}>
-        {products
-          .filter((_, i) => i < 9)
-          .map(({ id, name, price, img }) => {
+      <div style={{ position: "relative" }}>
+        <button
+          type="button"
+          style={{ display: scrollState.isScrollAtLeftMost ? "none" : "grid" }}
+          className={`${scss.controller} ${scss.prev}`}
+          onClick={goPrev}
+        >
+          <Lower className={scss.arrow} />
+        </button>
+        <ul className={scss.products} ref={productsRef}>
+          {suggestedProducts.map(({ id, name, price, img }) => {
             const isInFavourites = favourites.find(
               product => product.id === id
             );
@@ -133,7 +132,91 @@ const Suggestions = () => {
               </li>
             );
           })}
-      </ul>
+        </ul>
+        <button
+          type="button"
+          style={{ display: scrollState.isScrollAtRightMost ? "none" : "grid" }}
+          className={`${scss.controller} ${scss.next}`}
+          onClick={goNext}
+        >
+          <Greater className={scss.arrow} />
+        </button>
+      </div>
+    </section>
+  ) : (
+    <section className={scss.wrapper}>
+      <div className={scss.headlineBox}>
+        <h2 className={scss.headline}>You may also like</h2>
+        <Link to="/shop" className={scss.link}>
+          View more
+        </Link>
+      </div>
+      <div style={{ position: "relative" }}>
+        <button
+          type="button"
+          style={{ display: scrollState.isScrollAtLeftMost ? "none" : "grid" }}
+          className={`${scss.controller} ${scss.prev}`}
+          onClick={goPrev}
+        >
+          <Lower className={scss.arrow} />
+        </button>
+        <ul className={scss.products} ref={productsRef}>
+          {products
+            .filter((_, i) => i < 9)
+            .map(({ id, name, price, img }) => {
+              const isInFavourites = favourites.find(
+                product => product.id === id
+              );
+
+              return (
+                <li className={scss.productsItem} key={id}>
+                  <div className={scss.favouritesBox}>
+                    <button
+                      className={scss.favouritesBtn}
+                      type="button"
+                      onClick={() => dispatch(toggleFavourites(id))}
+                    >
+                      {isInFavourites ? (
+                        <FavouritesHeartFull className={scss.favouritesIcon} />
+                      ) : (
+                        <FavouritesHeartEmpty className={scss.favouritesIcon} />
+                      )}
+                    </button>
+                  </div>
+                  <Link
+                    to={{
+                      pathname: `/shop/${id}`,
+                      search: "?from=shop&size=48&quantity=1",
+                    }}
+                  >
+                    <div className={scss.productImgBox}>
+                      <img
+                        width={276}
+                        height={320}
+                        src={img}
+                        alt={name}
+                        className={scss.productImg}
+                        loading="lazy"
+                      />
+                    </div>
+                    <div className={scss.productInfoBox}>
+                      <p className={scss.productName}>{name}</p>
+                      <p className={scss.productPrice}>${price}</p>
+                    </div>
+                  </Link>
+                </li>
+              );
+            })}
+        </ul>
+        <button
+          type="button"
+          style={{ display: scrollState.isScrollAtRightMost ? "none" : "grid" }}
+          className={`${scss.controller} ${scss.next}`}
+          onClick={goNext}
+        >
+          <Greater className={scss.arrow} />
+        </button>
+      </div>
     </section>
   );
 };
